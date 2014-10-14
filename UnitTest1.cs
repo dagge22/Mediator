605e2c8da0ab9c65de418b3bfd78b8a5803d1689
dagge22 <dag.w@hotmail.com>
2014-10-14 13:24:06 +0200

    Fick ett konstigt error medelande men unit testet går att debugga utan problem

---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MediatorLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            ComputerSystem computerSystem = new ComputerSystem();
+            Computer computer = new Computer(computerSystem);
+            Keyboard keyboard = new Keyboard(computerSystem);
+            Screen screen = new Screen(computerSystem);
+
+            //Act
+            computer.SwitchOn();
+            keyboard.KeyPressed();
+            screen.DisplayCharacter();
+            //Assert
+
+        }
+    }
+}
